fe606e184727b9000628d524fbbf643e28d4d866
kroniak <me@kroniak.net>
2021-06-08 14:04:29 +0300

    add 20.10.7 version

--- build/build.sh
+++ build/build.sh
@@ -9,6 +9,7 @@ command -v docker >/dev/null 2>&1 || {
 SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build -t kroniak/docker-image:19.03.8 "$SCRIPT_ROOT/../19.03.8"
+docker build -t kroniak/docker-image:20.10.7 "$SCRIPT_ROOT/../20.10.7"
 docker build -t kroniak/docker-image:latest "$SCRIPT_ROOT/.."
 
 docker rmi -f $(docker images -q --filter "dangling=true")
